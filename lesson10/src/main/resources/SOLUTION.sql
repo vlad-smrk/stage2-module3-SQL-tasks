326793422f50029b7e9b57a90b1dd6aca3f3d606
Vladislav Samorukov <smrk.vld@gmail.com>
2023-04-19 20:15:24 +0300

    some fixes5

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,6 +1,2 @@
-CREATE LOCAL TEMPORARY TABLE Averages (subject_id bigint, average_mark int);
-INSERT INTO Averages (subject_id, average_mark) SElECT subject_id, AVG(mark) FROM Mark GROUP BY subject_id;
-SELECT * FROM Subject WHERE id = (SELECT subject_id FROM Averages WHERE average_mark = (SELECT MAX(average_mark) FROM Averages));
-CREATE LOCAL TEMPORARY TABLE Totals (student_id bigint, total_paid decimal);
-INSERT INTO Totals (student_id, total_paid) SELECT student_id, SUM(amount) FROM Payment GROUP BY student_id;
-SELECT * FROM Student WHERE id IN (SELECT student_id FROM Totals WHERE total_paid < (SELECT AVG(total_paid) FROM Totals));
+SELECT * FROM Subject WHERE id = (SELECT subject_id FROM (SELECT subject_id, AVG(mark) AS average_mark FROM Mark GROUP BY subject_id) WHERE average_mark = (SELECT MAX(average_mark) FROM (SELECT subject_id, AVG(mark) AS average_mark FROM Mark GROUP BY subject_id)));
+SELECT * FROM Student WHERE id IN (SELECT student_id FROM (SELECT student_id, SUM(amount) AS total_paid FROM Payment GROUP BY student_id) WHERE total_paid < (SELECT AVG(total_paid) FROM (SELECT student_id, SUM(amount) AS total_paid FROM Payment GROUP BY student_id)));
