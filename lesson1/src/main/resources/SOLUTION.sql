a07b535f5e686f6a1ed0cc675ae76f7f509d55ca
Vladislav Samorukov <smrk.vld@gmail.com>
2023-04-19 19:53:27 +0300

    some fixes34

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Student (id bigint PRIMARY KEY, name varchar(63), birthday date, groupnumber int);
 CREATE TABLE Subject (id bigint PRIMARY KEY, name varchar(63), description varchar(255), grade int);
 CREATE TABLE PaymentType (id bigint PRIMARY KEY, name varchar(63));
-CREATE TABLE Payment (id bigint PRIMARY KEY, type_id bigint FOREIGN KEY REFERENCES PaymentType(id), amount decimal, student_id bigint FOREIGN KEY REFERENCES Student(id), payment_date datetime);
-CREATE TABLE Mark (id bigint PRIMARY KEY, student_id bigint FOREIGN KEY REFERENCES Student(id), subject_id bigint FOREIGN KEY REFERENCES Subject(id), mark int);
+CREATE TABLE Payment (id bigint PRIMARY KEY, type_id bigint, amount decimal, student_id bigint, payment_date datetime, FOREIGN KEY (type_id) REFERENCES PaymentType(id), FOREIGN KEY (student_id) REFERENCES Student(id));
+CREATE TABLE Mark (id bigint PRIMARY KEY, student_id bigint, subject_id bigint, mark int, FOREIGN KEY (student_id) REFERENCES Student(id), FOREIGN KEY (subject_id) REFERENCES Subject(id));
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ SELECT * FROM Payment WHERE amount >= 500;
 SELECT * FROM Student WHERE CURRENT_TIMESTAMP >= DATEADD(year, 20, birthday);
 SELECT * FROM Student WHERE groupnumber = 10 AND CURRENT_TIMESTAMP < DATEADD(year, 20, birthday);
 SELECT * FROM Student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
-SELECT * FROM Payment WHERE CURRENT_TIMESTAMP <= DATEADD(month, 8, payment_date);
+SELECT * FROM Payment WHERE CURRENT_TIMESTAMP <= DATEADD(month, 14, payment_date);
 SELECT * FROM Student WHERE name LIKE 'A%';
 SELECT * FROM Student WHERE name LIKE 'Roxi%' AND groupnumber = 4 OR name LIKE 'Tallie%' AND groupnumber = 9;
