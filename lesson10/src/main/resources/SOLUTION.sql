8c3d0d68387191b7f68c89f4b7ad63d553452b11
Vladislav Samorukov <smrk.vld@gmail.com>
2023-05-24 21:23:03 +0300

    fixes in #2, #10, #12

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,3 @@
-SELECT * FROM Subject WHERE id = (SELECT subject_id FROM (SELECT subject_id, AVG(mark) AS average_mark FROM Mark GROUP BY subject_id) WHERE average_mark = (SELECT MAX(average_mark) FROM (SELECT subject_id, AVG(mark) AS average_mark FROM Mark GROUP BY subject_id)));
-SELECT * FROM Student WHERE id IN (SELECT student_id FROM (SELECT student_id, SUM(amount) AS total_paid FROM Payment GROUP BY student_id) WHERE total_paid < (SELECT AVG(total_paid) FROM (SELECT student_id, SUM(amount) AS total_paid FROM Payment GROUP BY student_id)));
+SELECT * FROM Subject INNER JOIN Mark ON Subject.id = subject_id GROUP BY Subject.id HAVING AVG(mark) > (SELECT AVG(mark) FROM Mark);
+
+SELECT * FROM Student INNER JOIN Payment ON Student.id = student_id GROUP BY Student.id HAVING SUM(amount) < (SELECT AVG(total_paid) FROM (SELECT SUM(amount) AS total_paid FROM Payment GROUP BY student_id));
--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM Student WHERE groupnumber >= 4;
-DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark < 4);
+DELETE FROM Student WHERE id IN (SELECT DISTINCT Student.id FROM Student INNER JOIN Mark ON Student.id = student_id INNER JOIN Subject ON Subject.id = subject_id WHERE grade >= 4);
+DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark WHERE mark < 4);
 DELETE FROM PaymentType WHERE name = 'DAILY';
 DELETE FROM Mark WHERE mark < 7;
